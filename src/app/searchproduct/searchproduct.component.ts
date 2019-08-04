cc49cb4e5d4f4705280658dd9b5c3a8f47ece2dd
roshniiee <36438818+roshniiee@users.noreply.github.com>
2019-08-05 01:53:48 +0530

    Add files via upload

---
+++ src/app/searchproduct/searchproduct.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Products } from '../Products';
+import { ProductserviceService } from '../productservice.service';
+
+@Component({
+  selector: 'app-searchproduct',
+  templateUrl: './searchproduct.component.html',
+  styleUrls: ['./searchproduct.component.css']
+})
+export class SearchproductComponent implements OnInit {
+Producttt:Products[]=[];
+pser:ProductserviceService;
+  constructor(pser:ProductserviceService) {
+    this.pser=pser;
+   }
+   search(p){
+    this.Producttt=this.pser.SearchData(p);
+  
+    }
+  ngOnInit() {
+  }
+ 
+
+}
